9450f15d3e0a9e3485375f5a494302d86833e6a6
Jamie13 <100997775@student.swin.edu.au>
2016-08-10 11:02:56 +1000

    font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -58,11 +58,8 @@ namespace CardGames
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
-<<<<<<< HEAD
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
-=======
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 251, 153);
->>>>>>> 1ba8a69f511e93183a8a7363c126a143dcc5770a
+				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
 			{
